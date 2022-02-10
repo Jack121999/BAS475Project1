80f5e42c5965c850c8ad5ce8a87bb3bbdb4d0e11
hdw34bp <82785244+hdw34bp@users.noreply.github.com>
2022-02-10 09:02:34 -0500

    Autoplot for certain region

--- app.R
+++ app.R
@@ -1,11 +1,15 @@
 library(shiny)
-
+library(fpp3)
+head(tourism)
 ui <- fluidPage(
-
+  plotOutput(outputId = "plotted_series")
 )
 
 server <- function(input, output, session) {
-
+  output$plotted_series <- renderPlot({
+    autoplot(tourism[tourism$Region == "Adelaide", ])
+  })
+  
 }
 
 shinyApp(ui, server)
